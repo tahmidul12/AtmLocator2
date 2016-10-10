575d781210da5bf34437cffb625b2166de4c08f4
shakil <shakil.softwindtech@gmail.com>
2016-10-10 12:26:41 +0600

    a simple bank spinner added as menuItem 4m shakil

--- app/src/main/java/com/atm/atmlocator/Offline.java
+++ app/src/main/java/com/atm/atmlocator/Offline.java
@@ -9,13 +9,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ListPopupWindow;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +37,11 @@ public class Offline extends AppCompatActivity implements LoaderManager.LoaderCa
     private int pageNo, backpressDo = 0, listViewPos = 0;
     private ArrayAdapterBank arrayAdapterBank;
     private ArrayAdapter<BankModel> adapter;
+
+    private String[] spinnerItems = new String[]{"All", "DBBL", "Brac Bank", "AB Bank"
+                                                 , "City Bank", "EBL", "Exim Bank", "HSBC"
+                                                 , "IFIC Bank", "One Bank", "Premier Bank", "Prime Bank"
+                                                 , "SCB", "UCBL", "South-East Bank"};
 
     private Cursor cursor;
     @Override
@@ -62,6 +74,10 @@ public class Offline extends AppCompatActivity implements LoaderManager.LoaderCa
 
     }
 
+    /*
+    when coming back to the offline bank detail user should scroll down to the same banklist item from where he go for detail
+    so he is set to the same position
+     */
     @Override
     protected void onResume() {
         listOfBank.setSelection(listViewPos);
@@ -88,6 +104,8 @@ public class Offline extends AppCompatActivity implements LoaderManager.LoaderCa
     }
 
     private void addDatatoList() {
+        if(listBank.size()>0)
+            listBank.clear();
         String bname, batmname, baddress, lat, longi, state, city;
         if (cursor.moveToFirst()) {
             do{
@@ -104,6 +122,82 @@ public class Offline extends AppCompatActivity implements LoaderManager.LoaderCa
             } while (cursor.moveToNext());
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_offline, menu);
+        MenuItem myActionMenuItem = menu.findItem( R.id.action_search);
+        MenuItem spinneritem = menu.findItem(R.id.spinner);
+        Spinner spinner = (Spinner) MenuItemCompat.getActionView(spinneritem);
+        ArrayAdapter<CharSequence> adapterSpinner = ArrayAdapter.createFromResource(this,
+                R.array.allBankName, android.R.layout.simple_spinner_item);
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapterSpinner);
+        spinner.setOnItemSelectedListener(new SpinnerItemSelectedListener());
+        View actionView = myActionMenuItem.getActionView();
+        //AutoCompleteTextView searchView = (AutoCompleteTextView) actionView.findViewById(R.id.action_search);
+        final SearchView searchView = (SearchView) myActionMenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchViewListener());
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class SearchViewListener implements SearchView.OnQueryTextListener{
+
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            if(newText.isEmpty() || newText.matches("") || newText.length() == 0)
+                addDatatoList();
+            filterData(newText);
+            return false;
+        }
+    }
+    private class SpinnerItemSelectedListener implements AdapterView.OnItemSelectedListener{
+
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            Toast.makeText(getApplicationContext(), "selected bank="+spinnerItems[i], Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    }
+
+    private void filterData(String query){
+        if(listBank != null)
+            listBank.clear();
+        String bname, batmname, baddress, lat, longi, state, city;
+        if (cursor.moveToFirst()) {
+            do{
+                bname = cursor.getString(cursor.getColumnIndex(AtmProvider.BANK));
+                batmname = cursor.getString(cursor.getColumnIndex(AtmProvider.ATM_NAME));
+                lat = cursor.getString(cursor.getColumnIndex(AtmProvider.LAT));
+                longi = cursor.getString(cursor.getColumnIndex(AtmProvider.LONGI));
+                baddress = cursor.getString(cursor.getColumnIndex(AtmProvider.ADDRESS));
+                city = cursor.getString(cursor.getColumnIndex(AtmProvider.CITY));
+                state = cursor.getString(cursor.getColumnIndex(AtmProvider.STATE));
+                if(baddress.toLowerCase().contains(query.toLowerCase()) || city.toLowerCase().contains(query.toLowerCase())
+                        || state.toLowerCase().contains(query.toLowerCase()))
+                      listBank.add(new BankModel(bname, batmname, lat, longi, baddress, city, state, null));
+                adapter.notifyDataSetChanged();
+            } while (cursor.moveToNext());
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
